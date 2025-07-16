b0fcedc4b029a16707c13dbdc723a6c9ebaef816
Bryan Carrillo <carrbryan0123@gmail.com>
2025-07-16 20:53:43 +0000

    implemented geoapify places api to search for gas stations along route

--- api/test.py
+++ api/test.py
@@ -1,10 +1,20 @@
 import geoapify as geo
 import tollguru as tg
 
-data = geo.get_route(27.9806068, -82.5151287, 29.6502046, -82.3416096)
-coords = geo.get_coordinates(data)
-props = geo.get_properties(data)
+#get raw json response for route data
+route_data = geo.get_route(27.9806068, -82.5151287, 28.428683, -81.306199)
 
-cost = tg.get_trip_cost(coords,props)
+#get coordinates and properties(distance/time)
+coords = geo.get_coordinates(route_data)
+props = geo.get_properties(route_data)
 
-print(cost)
+#get trip costs using tollguru
+cost = tg.get_trip_cost(coords, props)
+
+#find gas stations along route
+fuel_stations = geo.get_fuel_stations_along_route(route_data)
+fuel_coords = geo.get_fuel_coordinates(fuel_stations)
+print(fuel_coords)
+
+#print summary of gas stations found
+geo.print_stations_summary(fuel_stations)
